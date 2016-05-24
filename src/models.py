67dba03092115e45c880c8feb6e51b68d7acbe9a
nishithbsk <nishith@stanford.edu>
2016-05-24 14:16:51 -0700

    Added barebones of LSTM

---
+++ src/models.py
@@ -0,0 +1,44 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.python.framework import dtypes
+from tensorflow.contrib import learn
+
+def lstm_model(time_steps, rnn_layers, dense_layers=None):
+    """
+    Creates a deep model based on:
+        * stacked lstm cells
+        * an optional dense layers
+    :param time_steps: the number of time steps the model will be looking at.
+    :param rnn_layers: list of int or dict
+                         * list of int: the steps used to instantiate the `BasicLSTMCell` cell
+                         * list of dict: [{steps: int, keep_prob: int}, ...]
+    :param dense_layers: list of nodes for each layer
+    :return: the model definition
+    """
+    def lstm_cells(layers):
+        if isinstance(layers[0], dict):
+            return [tf.nn.rnn_cell.DropoutWrapper(tf.nn.rnn_cell.BasicLSTMCell(layer['steps']),
+                                                  layer['keep_prob'])
+                    if layer.get('keep_prob') else tf.nn.rnn_cell.BasicLSTMCell(layer['steps'])
+                    for layer in layers]
+        return [tf.nn.rnn_cell.BasicLSTMCell(steps) for steps in layers]
+
+    def dnn_layers(input_layers, layers):
+        if layers and isinstance(layers, dict):
+            return learn.ops.dnn(input_layers,
+                                 layers['layers'],
+                                 activation=layers.get('activation'),
+                                 dropout=layers.get('dropout'))
+        elif layers:
+            return learn.ops.dnn(input_layers, layers)
+        else:
+            return input_layers
+
+    def _lstm_model(X, y):
+        stacked_lstm = tf.nn.rnn_cell.MultiRNNCell(lstm_cells(rnn_layers))
+        x_ = learn.ops.split_squeeze(1, time_steps, X)
+        output, layers = tf.nn.rnn(stacked_lstm, x_, dtype=dtypes.float32)
+        output = dnn_layers(output[-1], dense_layers)
+        return learn.models.linear_regression(output, y)
+
+    return _lstm_model
