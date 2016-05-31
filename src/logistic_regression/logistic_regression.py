5cf8c77b2ba77b36c5474cb1389047ce4495e131
Amanda Chow <aychow256@gmail.com>
2016-05-30 17:43:42 -0700

    logistic regression baseline

--- src/logistic_regression/logistic_regression.py
+++ src/logistic_regression/logistic_regression.py
@@ -1,5 +1,35 @@
 import numpy as np
+from sklearn.linear_model import LogisticRegression
 
-def load_data(data_path='../../data/processed/logistic_regression/uganda.csv'):
+def load_data(data_path='../../data/processed/logistic_regression/uganda_data.npy'):
     # returns tuple (X_train, X_test, y_train, y_test)
     return np.load(data_path)
+
+X_train, X_test, y_train, y_test = load_data()
+
+
+model = LogisticRegression()
+model.fit(X_train, y_train)
+pred = model.predict(X_test)
+
+precision_num, precision_denom = 0.0, 0.0
+for i in range(len(pred)):
+	if y_test[i] == 1:
+		if pred[i] == 1: 
+			precision_num += 1
+			precision_denom += 1
+	else:
+		if pred[i] == 1: precision_denom += 1
+print float(precision_num)/precision_denom
+
+recall_num, recall_denom = 0.0, 0.0
+for i in range(len(pred)):
+	if y_test[i] == 1:
+		if pred[i] == 1: 
+			recall_num += 1
+			recall_denom += 1
+		else:
+			recall_denom += 1
+
+print float(recall_num)/recall_denom
+
